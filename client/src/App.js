977763b03028b73208197b8355311ff434895729
Adibab <adiba.du.bd@gmail.com>
2023-03-14 12:09:54 +0000

    send validation email to users

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import { Route, Routes } from "react-router-dom";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import VerifyCertificateNum from "../components/users/VerifyCertificateNum";
 
-const App = () => (
-	<Routes>
+function App (){
+	return (
+		<div>
+			{/* <Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
-	</Routes>
-);
+		
+	</Routes> */}
+	{/* <About/>
+	<Home /> */}
+	<VerifyCertificateNum/>
+		</div>
+	);
+}
 
 export default App;
--- server/api.js
+++ server/api.js
@@ -1,12 +1,42 @@
 import { Router } from "express";
-
 import logger from "./utils/logger";
-
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const router = Router();
+const MAILGUN_DOMAIN = "sandbox02afd029aa3943e8b405f995365c67b8.mailgun.org";
+const MAILGUN_API_KEY = "4847b7149dd9d399482ac14d3a78f82e-b36d2969-1f8ba3ad";
 
-router.get("/", (_, res) => {
-	logger.debug("Welcoming everyone...");
-	res.json({ message: "Hello, world!" });
+const mg = require("mailgun-js");
+
+const mailgun = () =>
+	mg({
+		apiKey: MAILGUN_API_KEY,
+		domain: MAILGUN_DOMAIN,
+	});
+
+
+
+router.post("/", (request, response) => {
+
+	const textToEveryOne = `Your email has been verified Click Here`;
+	const { email, certificateNum } = request.body;
+	const messageData = {
+		from: "adiba.fin@gmail.com",
+		to: `${email}`,
+		certificateNum: `${certificateNum}`,
+		text: `<p> ${textToEveryOne} </p>`,
+	};
+	mailgun().messages().send(messageData, (error, body) => {
+		if (error) {
+			console.log(error);
+			response.status(500).send({ message: "Error in sending email" });
+		} else {
+			console.log(body);
+			response.send({ message: "Email sent successfully" });
+		}
+	});
 });
 
 export default router;
