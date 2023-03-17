1e0fb13e2ca7a1d14e262455eb660edd3b3b7bdc
Adibab <adiba.du.bd@gmail.com>
2023-03-17 06:17:16 +0000

    testing app.js

--- server/api.js
+++ server/api.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import db from "./db";
 import logger from "./utils/logger";
 
 const router = Router();
@@ -9,4 +9,13 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
+
+router.get("/users", (req, res) => {
+	db.query("select * from  accounts")
+		.then((accounts) => res.status(200).json(accounts.rows))
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send(err);
+		});
+});
 export default router;
