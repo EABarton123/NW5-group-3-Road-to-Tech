67f1d72a7cd8e9f6bfbb0ab2f0a6136f23cf0148
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-03-18 13:21:29 +0000

    Add the form label a control.

--- client/src/components/Pages/signup/Signup.js
+++ client/src/components/Pages/signup/Signup.js
@@ -5,19 +5,19 @@ import "./Signup.css";
 export const Signup = () => {
   const [subFormDate, setSubFormDate] = useState({
     email:"",
-    certificateNumber: "",
-    userName:"",
+    certificateNum: "",
+    username:"",
     password:"" });
 
     const signupSubmit =(e) =>{
       e.preventDefault();
       console.log("clicked");
 
-    alert(` Email ${ subFormDate.email}, Certificate Number: ${subFormDate.certificateNumber}, UserName: ${subFormDate.userName},  Password: ${ subFormDate.password},`);
+    alert(` Email ${ subFormDate.email}, Certificate Number: ${subFormDate.certificateNum}, UserName: ${subFormDate.username},  Password: ${ subFormDate.password},`);
     setSubFormDate({
       email:"",
-      certificateNumber: "",
-      userName:"",
+      certificateNum: "",
+      username:"",
       password:"" });
   };
     return (
@@ -25,25 +25,25 @@ export const Signup = () => {
      <form onSubmit={(e) => signupSubmit(e)}>
       <h1> Graduete SIGN UP</h1>
       <div>
-    <label> Email: </label>
-    <input type="text" placeholder='Email' value={subFormDate.email} onChange = {(e) => setSubFormDate({
+    <label htmlFor="email"> Email: </label>
+    <input type="text" placeholder='Email' name="email" required value={subFormDate.email} onChange = {(e) => setSubFormDate({
   ...subFormDate, email:e.target.value })} />
     </div>
 
     <div>
-    <label> Certificate Number:</label>
-    <input type="number" placeholder='Certificate Number' value={subFormDate.certificateNumber} onChange = {(e) => setSubFormDate({
-  ...subFormDate, certificateNumber:e.target.value })} />
+    <label htmlFor="certificateNum"> Certificate Number:</label>
+    <input type="number" placeholder='Certificate Number'name="certificateNum" required value={subFormDate.certificateNum} onChange = {(e) => setSubFormDate({
+  ...subFormDate, certificateNum:e.target.value })} />
     </div>
 
     <div>
-    <label> UserName:</label>
-    <input type="text" placeholder='UserName' value={subFormDate.userName} onChange = {(e) => setSubFormDate({
-  ...subFormDate, userName:e.target.value })} />
+    <label htmlFor="username"> UserName:</label>
+    <input type="text" placeholder='username' name="username" required value={subFormDate.username} onChange = {(e) => setSubFormDate({
+  ...subFormDate, username:e.target.value })} />
     </div>
     <div>
-    <label>  Password:</label>
-    <input type="password" placeholder="Password" value={subFormDate. password} onChange = {(e) => setSubFormDate({
+    <label htmlFor = "password">  Password:</label>
+    <input type="password" placeholder="password" name="password" required value={subFormDate. password} onChange = {(e) => setSubFormDate({
   ...subFormDate,password:e.target.value })} />
     </div>
     <Button />
