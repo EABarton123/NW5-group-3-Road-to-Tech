2473bd65a8bf84c97fba8b5c4711b6b06c7af13b
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-03-18 18:15:06 +0000

    menu item

--- client/src/components/menuItem.js
+++ client/src/components/menuItem.js
@@ -1,13 +1,13 @@
-// import React from "react";
-// import "../assets/css/menuItem.css";
-// import "../assets/css/customMenu.css";
+import React from "react";
+import "../assets/css/menuItem.css";
+import "../assets/css/customMenu.css";
 
-// function MenuItem({ menuText }) {
-//    return (
-//     <div className="Box">
-//       <div className="box1"> <a href="menu">{menuText}</a></div>
-//     </div>
-//   );
-// }
+function MenuItem({ menuText }) {
+   return (
+    <div className="Box">
+      <div className="box1"> <a href="menu">{menuText}</a></div>
+    </div>
+  );
+}
 
-// export default MenuItem;
+export default MenuItem;
