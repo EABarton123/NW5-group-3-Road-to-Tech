3078bd16edabc12d5c8dd66d4d6bbfe31b141f2f
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-03-18 19:12:48 +0000

    log-in-panel

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,31 +1,31 @@
-// import React from "react";
-// import "../assets/css/login.css";
-// import login_sub_image from "../assets/css/outreach.jpg";
-// function Login() {
-//   return (
-//     <>
-//     <div className='login_image_box'>
-//     <form >
-//         <div class="container">
-//         <label for="uname"><b>Username</b></label>
-//          <input type="text" placeholder="Enter Username" name="uname" required />
-//          <label for="psw"><b>Password</b></label>
-//     <input type="password" placeholder="Enter Password" name="psw" required></input>
-//     <button type="submit">Login</button>
-//     <label>
-//       <input type="checkbox" checked="checked" name="remember"/> Remember me
-//     </label>
-//         </div>
-//         <div class="container" >
-//     <button type="button" className="cancelbtn">Cancel</button>
-//     <span class="psw">Forgot <a href="#">password?</a></span>
-//   </div>
-//     </form>
-//     <img src={login_sub_image} height={200}></img>
-//     </div>
+import React from "react";
+import "../assets/css/login.css";
+import login_sub_image from "../assets/css/outreach.jpg";
+function Login() {
+  return (
+    <>
+    <div className='login_image_box'>
+    <form >
+        <div class="container">
+        <label for="uname"><b>Username</b></label>
+         <input type="text" placeholder="Enter Username" name="uname" required />
+         <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required></input>
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"/> Remember me
+    </label>
+        </div>
+        <div class="container" >
+    <button type="button" className="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+    </form>
+    <img src={login_sub_image} height={200}></img>
+    </div>
 
-//     </>
-//   );
-// }
+    </>
+  );
+}
 
-// export default Login;
+export default Login;
