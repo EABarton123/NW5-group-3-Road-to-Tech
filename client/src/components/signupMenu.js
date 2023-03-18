c6de00d8fcfe9d9ba2b6d05bff5ef6f218e77fe7
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-03-18 18:16:18 +0000

    sign up menu

--- client/src/components/signupMenu.js
+++ client/src/components/signupMenu.js
@@ -1,17 +1,17 @@
-// import React from "react";
-// import "../assets/css/signupMenu.css";
-// function SignupMenu() {
-//   return (
+import React from "react";
+import "../assets/css/signupMenu.css";
+function SignupMenu() {
+  return (
 
-//     <div className="dropdown">
-//   <button className="dropbtn">SIGNUP</button>
-//   <div className="dropdown-content">
-//     <a href="graduates">GRADUATES</a>
-//     <a href="employabilityteam">EMPLOYABILITY TEAM</a>
-//     </div>
+    <div className="dropdown">
+  <button className="dropbtn">SIGNUP</button>
+  <div className="dropdown-content">
+    <a href="graduates">GRADUATES</a>
+    <a href="employabilityteam">EMPLOYABILITY TEAM</a>
+    </div>
 
-// </div>
-//   );
-// }
+</div>
+  );
+}
 
-// export default SignupMenu;
+export default SignupMenu;
