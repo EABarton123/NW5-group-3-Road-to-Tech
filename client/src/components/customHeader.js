56f140d21833094b0fdbfdecb1e3f81b147c8718
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-03-18 18:14:10 +0000

    custom header

--- client/src/components/customHeader.js
+++ client/src/components/customHeader.js
@@ -1,23 +1,23 @@
-// import  MenuItem  from "./menuItem";
-// import  SignupMenu  from "./signupMenu";
-// import React from "react";
-// import "../assets/css/menu.css";
-// import "../assets/css/customMenu.css";
-// import "../assets/css/menuItem.css";
-// import logo from "../assets/css/cyf_logo.png";
-// // import { width } from "@material-ui/system";
-// function CustomHeader( { MenuItemTexts }) {
-//   return(
-//     <div className='MenuRow'>
-//          <img style={{ margin:45,padding:0,borderRadius:15 }}
-//          width={288} height={70} src={logo} alt="Logo"></img>
-//     {
-//     MenuItemTexts.map((item) => {
-//        return <div className='Box' key={item} id={item}><MenuItem menuText={item} /></div>;
-//       })
-//     }
-//     <SignupMenu />
-//     </div>
-//   );
-// }
-// export default CustomHeader;
+import  MenuItem  from "./menuItem";
+import  SignupMenu  from "./signupMenu";
+import React from "react";
+import "../assets/css/menu.css";
+import "../assets/css/customMenu.css";
+import "../assets/css/menuItem.css";
+import logo from "../assets/css/cyf_logo.png";
+// import { width } from "@material-ui/system";
+function CustomHeader( { MenuItemTexts }) {
+  return(
+    <div className='MenuRow'>
+         <img style={{ margin:45,padding:0,borderRadius:15 }}
+         width={288} height={70} src={logo} alt="Logo"></img>
+    {
+    MenuItemTexts.map((item) => {
+       return <div className='Box' key={item} id={item}><MenuItem menuText={item} /></div>;
+      })
+    }
+    <SignupMenu />
+    </div>
+  );
+}
+export default CustomHeader;
