e23013f0e9aedcefc268af0f916852cc98c17063
EABarton123 <83503987+EABarton123@users.noreply.github.com>
2023-03-20 22:43:08 +0000

    app deleted

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,30 @@ import { Route, Routes } from "react-router-dom";
 import React, { Component } from "react";
 import "./App.css";
 import CustomHeader from "./components/customHeader"; //Include Heder
-
-class App extends Component {
-	MenuItemTexts = ["STORIES", "INTERVIEW TIPS", "GRADUATE RESOURCES"];
-	render() {
-		return (
-			<div className="maincontainer">
-				<CustomHeader MenuItemTexts={this.MenuItemTexts}></CustomHeader>
-			</div>
-		);
-	}
-}
-=========
 import About from "./pages/About";
 import Home from "./pages/Home";
 import AdminJobs from "./pages/AdminJobs";
 import GradJobs from "./pages/GradJobs";
 
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about" element={<About />} />
-		<Route path="/admin" element={<AdminJobs />} />
-		<Route path="/grad" element={<GradJobs />} />
-	</Routes>
-);
->>>>>>>>> Temporary merge branch 2
+class App extends Component {
+	MenuItemTexts = ["STORIES", "INTERVIEW TIPS", "GRADUATE RESOURCES"];
+	render() {
+		return (
+			<div>
+				<div className="maincontainer">
+					<CustomHeader MenuItemTexts={this.MenuItemTexts}></CustomHeader>
+				</div>
+				<div>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/admin" element={<AdminJobs />} />
+						<Route path="/grad" element={<GradJobs />} />
+					</Routes>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default App;
