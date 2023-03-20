245a52315413165560a8bc6c24bc5cb2a0bf3148
EABarton123 <83503987+EABarton123@users.noreply.github.com>
2023-03-20 12:58:20 +0000

    updated gradJobs

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import AdminJobs from "./pages/AdminJobs";
-import GradJobs from "./pages/gradJobs";
+import GradJobs from "./pages/GradJobs";
 
 const App = () => (
 	<Routes>
