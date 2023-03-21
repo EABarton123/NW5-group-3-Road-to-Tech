1a8770d691d19c518661af11c236d183d0794021
EABarton123 <83503987+EABarton123@users.noreply.github.com>
2023-03-21 07:32:18 +0000

    app.js mergeconflict resolved

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 import JobForm from "./pages/JobForm/JobForm";
+import React, { Component } from "react";
+import "./App.css";
+import CustomHeader from "./components/customHeader"; //Include Heder
 import About from "./pages/About";
 import Home from "./pages/Home";
-
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/job" element={<JobForm />} />
-	</Routes>
-);
+import AdminJobs from "./pages/AdminJobs";
+import GradJobs from "./pages/GradJobs";
+class App extends Component {
+	MenuItemTexts = ["STORIES", "INTERVIEW TIPS", "GRADUATE RESOURCES"];
+	render() {
+		return (
+			<div>
+				<div className="maincontainer">
+					<CustomHeader MenuItemTexts={this.MenuItemTexts}></CustomHeader>
+				</div>
+				<div>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/admin" element={<AdminJobs />} />
+						<Route path="/grad" element={<GradJobs />} />
+						<Route path="/job" element={<JobForm />} />
+					</Routes>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default App;
