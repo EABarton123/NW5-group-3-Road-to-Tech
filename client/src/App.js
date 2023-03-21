b8e6568540275ca94a1203f2e80c0785bf218637
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-03-21 09:59:20 +0000

    alt img

--- client/src/App.js
+++ client/src/App.js
@@ -25,3 +25,57 @@ class App extends Component {
 }
 
 export default App;
+
+// import React,{ Component } from "react";
+// // import "./App.css";
+
+// import  Slide  from "./components/slide";
+// import  Login  from "./components/login";
+// import "./assets/css/content.css";
+
+// class App extends Component {
+
+// 	render() {
+// 		return (
+
+// 					<Routes>
+// 						<Route path="/" element={<Home />} />
+// 						<Route path="/about" element={<About />} />
+// 					</Routes>
+
+// 		);
+// 	}
+// }
+
+// export default App;
+
+// // import React, { Component } from "react";
+// // import { Routes, Route } from "react-router-dom";
+// // import Slide from "./components/slide";
+// // import Login from "./components/login";
+// // import Home from "./home";
+// // import About from "./about";
+// // import "./assets/css/content.css";
+
+// // class App extends Component {
+// // 	render() {
+// // 		return (
+// // 			<>
+// // 				<div className="maincontainer">
+// // 					<hr />
+// // 					<div className="content">
+// // 						<Slide />
+// // 						<Login />
+// // 						<Routes>
+// // 							<Route path="/" element={<Home />} />
+// // 							<Route path="/about" element={<About />} />
+// // 						</Routes>
+// // 					</div>
+// // 					<hr />
+// // 				</div>
+// // 			</>
+// // 		);
+// // 	}
+// // }
+
+// // export default App;
--- client/src/components/slide.js
+++ client/src/components/slide.js
@@ -2,14 +2,17 @@ import React from "react";
 import side_image from "../assets/css/cititec-class.jpg";
 import "../assets/css/content.css";
 function Slide() {
-  return (
-    <div className='content'>
-       <img className='image_content' src={side_image}
-
-
-         width={170} height={70} ></img>
-    </div>
-  );
+	return (
+		<div className="content">
+			<img
+				className="image_content"
+				src={side_image}
+				alt="content"
+				width={170}
+				height={70}
+			></img>
+		</div>
+	);
 }
 
 export default Slide;
