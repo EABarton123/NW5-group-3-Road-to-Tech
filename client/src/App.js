cd80d846d485dab442ba4e366a6c41089efcab87
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-03-23 14:21:53 +0000

    add css in the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ProfileS from "./components/pages/Profile/ProfileS";
-import DownloadCV from "../src/components/pages/DownloadCV/DownloadCV";
   import EducationsForm from "./components/pages/EducationsForm/EducationsForm.js";
 import SkillsForm from "./components/pages/SkillsForm/SkillsForm";
 
@@ -11,7 +10,7 @@ import SkillsForm from "./components/pages/SkillsForm/SkillsForm";
 const App = () => (
 <>
      <ProfileS />
-     <DownloadCV />
+
      <EducationsForm />
      <SkillsForm />
 </>
