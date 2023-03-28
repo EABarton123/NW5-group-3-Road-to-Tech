ee9dc1a6c100269c2a9eb9045e83831815a4585f
EABarton123 <83503987+EABarton123@users.noreply.github.com>
2023-03-28 21:37:04 +0100

    70 link the backend with front end react (#77)
    * test to see if backend works
    
    * test backend
    
    * deleted /
    
    * connected backend to sign up grad-render
    
    * added /verify on api
    
    * navigated from verify to sign up
    
    * lint ran

--- client/src/pages/SignUpAdmin/Signup.js
+++ client/src/pages/SignUpAdmin/Signup.js
@@ -20,7 +20,7 @@ export const Signup = () => {
 		// console.log("subFormDate");
 		if (
 			axios
-				.post("http://localhost:3000/api/signup", subFormDate)
+				.post("https://starter-kit-nqe2.onrender.com/api/signup", subFormDate)
 				.then((res) => {
 					console.log(res.data);
 					alert("Signup successful!");
@@ -33,7 +33,7 @@ export const Signup = () => {
 					});
 				})
 		) {
-			navigate("/Home");
+			navigate("/grad");
 		}
 		// .catch((err) => {
 		// 	console.error(err);
@@ -41,7 +41,7 @@ export const Signup = () => {
 		// })
 	};
 	return (
-		<div className="form-conteiner">
+		<div className="form-container">
 			<form onSubmit={signupSubmit}>
 				<h1> Sign Up</h1>
 				<div>
